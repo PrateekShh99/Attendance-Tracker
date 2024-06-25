0c7a225ac421f314a76464ad0ba967f2d43fb4e0
PratKing99 <89543431+PratKing99@users.noreply.github.com>
2024-06-25 20:10:03 +0530

    Updated default value
    if no option is selected , automatically marked present

--- Static/JavaScript.js
+++ Static/JavaScript.js
@@ -14,3 +14,19 @@ document.getElementById('attendanceForm').addEventListener('submit', function(ev
         body: JSON.stringify({ attendance: attendanceArray })
     });
 });
+
+window.onload = function() {
+    let radios = document.getElementsById('attendanceForm');
+    let isSelected = false;
+
+    for (let radio of radios) {
+        if (radio.checked) {
+            isSelected = true;
+            break;
+        }
+    }
+
+    if (!isSelected) {
+        radios[0].checked = true;
+    }
+}
